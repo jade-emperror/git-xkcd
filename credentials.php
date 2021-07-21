337da38c15ab72e3a145cbc7eac2ec016235e17a
jade-emperror <maestrobharath@gmail.com>
2021-07-21 21:06:43 +0530

    deploy 5 added credentials

--- credentials.php
+++ credentials.php
@@ -1,10 +1,10 @@
 <?php
-$dbhost='host';
-$dbuser ='user';
-$dbpwd ='password';
-$db='database_name';
-$from_mail_id='from@domain.com';
-$from_mail_pwd='password';
+$dbhost="host";
+$dbuser ="user";
+$dbpwd ="password";
+$db="database_name";
+$from_mail_id="from@domain.com";
+$from_mail_pwd="password";
 $smtp_port=000;
-$smtp_host='smtp.domain.com';
+$smtp_host="smtp.domain.com";
 ?>
